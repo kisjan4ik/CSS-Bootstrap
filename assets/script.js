d3d9fdc7869c0bfb09a11d81f534a27dbf3835b8
Turetska <oturetska@yahoo.com>
2020-06-29 21:32:54 -0400

    added functiomality to the button with cony=tact info and links to Github and LinkedIn

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,10 @@
-$("#contact").on("click", function (event) {
+$("#buttonn").one("click", function (event) {
     event.preventDefault();
 
-    var GitHub = $("<a>").text("Github: " + "<a>", "href", "https://github.com/kisjan4ik");
-    var LinkedIn = $("<a>").text("LinkedIn: ");
-    var email = $("<a>").text("Email: " + "oturetska@yahoo.com");
-    var phone = $("<a>").text("Phone #: " + "407-308-16-72")
 
-    $("#contactinfo").append(GitHub, "<br>", LinkedIn, "<br>", email, "<br>", phone)
-    console.log(GitHub);
-    console.log(LinkedIn);
-    console.log(email);
-    console.log(phone);
-    
+    $("#contactinfo").append("<div> <a href='https://github.com/kisjan4ik' target='_blank'> Github </a> </div>");
+    $("#contactinfo").append("<div> <a href='https://www.linkedin.com/in/olena-turetska-82126a191/' target='_blank'> LinkedIn </a> </div>");
+    $("#contactinfo").append("<div> Email: oturetska@yahoo.com </a> </div>");
+    $("#contactinfo").append("<div> Phone #: 407-308-16-72 </a> </div>");
+
 })
