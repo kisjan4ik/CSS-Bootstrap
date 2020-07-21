59b02495b79755ca5a9d50e9d881156af074c2a2
Turetska <oturetska@yahoo.com>
2020-07-21 18:17:57 -0400

    functionality to contact info button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
-$("#buttonn").one("click", function (event) {
+$("#buttonn").on("click", function (event) {
     event.preventDefault();
+
+    if($('#contactinfo').html().length > 0) {
+        $("#contactinfo").html(null)
+     }
+     else {
 
 
     $("#contactinfo").append("<div> <a href='https://github.com/kisjan4ik' target='_blank'> Github </a> </div>");
     $("#contactinfo").append("<div> <a href='https://www.linkedin.com/in/olena-turetska-82126a191/' target='_blank'> LinkedIn </a> </div>");
     $("#contactinfo").append("<div> Email: oturetska@yahoo.com </a> </div>");
     $("#contactinfo").append("<div> Phone #: 407-308-16-72 </a> </div>");
-
+     }
 })
